947dc75a8b69eefffd4730e4119490d29313ddd4
Mazharul Sabbir <mazharulsabbir2@gmail.com>
2022-10-10 10:47:50 +0600

    Initial commit

---
+++ app/src/main/java/com/mazharulsabbir/smsbroadcastreceiver/service/SmsBroadcastReceiver.kt
@@ -0,0 +1,28 @@
+package com.mazharulsabbir.smsbroadcastreceiver.service
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.provider.Telephony
+import android.util.Log
+
+
+class SmsBroadcastReceiver : BroadcastReceiver() {
+    override fun onReceive(context: Context?, intent: Intent?) {
+        Log.i(TAG, "onReceive: ${intent?.action}")
+
+        if (intent?.action.equals(Telephony.Sms.Intents.SMS_RECEIVED_ACTION)) {
+            val msgFromIntent = Telephony.Sms.Intents.getMessagesFromIntent(intent)
+            msgFromIntent.forEachIndexed { index, smsMessage ->
+                Log.i(TAG, "onReceive: $index")
+
+                Log.i(TAG, "Address: ${smsMessage.originatingAddress}")
+                Log.i(TAG, "Body: ${smsMessage.displayMessageBody}")
+            }
+        }
+    }
+
+    companion object {
+        private const val TAG = "SmsBroadcastReceiver"
+    }
+}
