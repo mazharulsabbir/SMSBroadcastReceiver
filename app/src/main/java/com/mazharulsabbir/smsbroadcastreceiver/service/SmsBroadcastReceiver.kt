db9b94661632c8f8383a96c292032d967bf38e1c
Mazharul Sabbir <mazharulsabbir2@gmail.com>
2022-10-10 17:47:32 +0600

    feat: api call on broadcast received

--- app/src/main/java/com/mazharulsabbir/smsbroadcastreceiver/service/SmsBroadcastReceiver.kt
+++ app/src/main/java/com/mazharulsabbir/smsbroadcastreceiver/service/SmsBroadcastReceiver.kt
@@ -5,7 +5,13 @@ import android.content.Context
 import android.content.Intent
 import android.provider.Telephony
 import android.util.Log
-
+import com.google.gson.GsonBuilder
+import com.google.gson.JsonParser
+import kotlinx.coroutines.*
+import org.json.JSONObject
+import java.io.OutputStreamWriter
+import java.net.HttpURLConnection
+import java.net.URL
 
 class SmsBroadcastReceiver : BroadcastReceiver() {
     override fun onReceive(context: Context?, intent: Intent?) {
@@ -18,6 +24,50 @@ class SmsBroadcastReceiver : BroadcastReceiver() {
 
                 Log.i(TAG, "Address: ${smsMessage.originatingAddress}")
                 Log.i(TAG, "Body: ${smsMessage.displayMessageBody}")
+            }
+            rawJSON()
+        }
+    }
+
+    @OptIn(DelicateCoroutinesApi::class)
+    fun rawJSON() {
+        // Create JSON using JSONObject
+        val jsonObject = JSONObject()
+        jsonObject.put("name", "Jack")
+        jsonObject.put("salary", "3540")
+        jsonObject.put("age", "23")
+
+        // Convert JSONObject to String
+        val jsonObjectString = jsonObject.toString()
+
+        GlobalScope.launch(Dispatchers.IO) {
+            val url = URL("http://dummy.restapiexample.com/api/v1/create")
+            val httpURLConnection = url.openConnection() as HttpURLConnection
+            httpURLConnection.requestMethod = "POST"
+            httpURLConnection.setRequestProperty("Content-Type", "application/json") // The format of the content we're sending to the server
+            httpURLConnection.setRequestProperty("Accept", "application/json") // The format of response we want to get from the server
+            httpURLConnection.doInput = true
+            httpURLConnection.doOutput = true
+
+            // Send the JSON we created
+            val outputStreamWriter = OutputStreamWriter(httpURLConnection.outputStream)
+            outputStreamWriter.write(jsonObjectString)
+            outputStreamWriter.flush()
+
+            // Check if the connection is successful
+            val responseCode = httpURLConnection.responseCode
+            if (responseCode == HttpURLConnection.HTTP_OK) {
+                val response = httpURLConnection.inputStream.bufferedReader()
+                    .use { it.readText() }  // defaults to UTF-8
+                withContext(Dispatchers.Main) {
+
+                    // Convert raw JSON to pretty JSON using GSON library
+                    val gson = GsonBuilder().setPrettyPrinting().create()
+                    val prettyJson = gson.toJson(JsonParser.parseString(response))
+                    Log.d(TAG, prettyJson)
+                }
+            } else {
+                Log.e(TAG, responseCode.toString())
             }
         }
     }
